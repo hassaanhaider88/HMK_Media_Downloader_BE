a640ebb7ecdf15a6c625f83ecc6f467dd03e6f49
hassaanhaider88 <hassaanhaider088@gmail.com>
2024-09-14 09:17:32 +0500

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
 var YTRouter = require('./routes/youtubeRouter.js');
 // var FSRouter  = require('./routes/fsRouter.js');
 var TWTRouter = require('./routes/twiterRouter.js');
 var TTRouter = require('./routes/tiktokRouter.js');
 var InstagramRouter = require('./routes/Instagram.js')
-var FaceBookRouter = require('./routes/faceBook.js')
+var FaceBookRouter = require('./routes/faceBook.js');
+// var dotnev = require('dotenv');
+
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const Port = process.env.PORT;
 app.use(cors());
-
 
 app.set('view engine', 'ejs');
 
@@ -19,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.send('index is working ......')
 });
 
 app.use('/download/tiktok',TTRouter);
@@ -30,8 +33,8 @@ app.use('/download/facebook',FaceBookRouter)
 //   // https://youtube.com/shorts/El5tSoMeRDw?si=rIN7qFy-6dV5exy1
 
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(Port, () => {
+  console.log(`Server is running on http://localhost:${Port}`);
 });
 
 
