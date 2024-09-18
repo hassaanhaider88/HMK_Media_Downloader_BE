d0be627bf77b5de1883c47e858934080455a6b2f
hassaanhaider88 <hassaanhaider088@gmail.com>
2024-09-18 07:04:15 +0500

    NOTHING BUT IMPROVEMENT

--- index.js
+++ index.js
@@ -5,35 +5,21 @@ var TWTRouter = require('./routes/twiterRouter.js');
 var TTRouter = require('./routes/tiktokRouter.js');
 var InstagramRouter = require('./routes/Instagram.js')
 var FaceBookRouter = require('./routes/faceBook.js');
-
 const cors = require('cors');
-
 const app = express();
 const Port = process.env.PORT || 3000;
 app.use(cors());
-
-app.set('view engine', 'ejs');
-
-app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.get('/', (req, res) => {
   res.send('index is working ......')
 });
-
 app.use('/download/tiktok',TTRouter);
-app.use('/download/youtube', YTRouter);
+app.use('/download/youtube', YTRouter); //https://www.youtube.com/watch?v=0HuOyleXVwk
 app.use('/download/twitter',TWTRouter);
 app.use('/download/instagram',InstagramRouter);
 app.use('/download/facebook',FaceBookRouter)
-//   // https://youtube.com/shorts/El5tSoMeRDw?si=rIN7qFy-6dV5exy1
-
-
 app.listen(Port, () => {
   console.log(`Server is running on http://localhost:${Port}`);
 });
-
-
-
 module.exports = app;
