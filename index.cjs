d15fcb29e1dd99d3b20f3f96e9652fa8f23629fb
hassaanhaider88 <hassaanhaider088@gmail.com>
2024-09-20 08:12:23 +0500

    changes

--- index.cjs
+++ index.cjs
@@ -1,32 +1,29 @@
-const dotenv = require('dotenv');
-const express = require('express');
-const cors = require('cors');
+import dotenv from 'dotenv';
+import express  from 'express';
+// import serverless from 'serverless-http';
+import YTRouter  from './routes/youtubeRouter.js';
+import TWTRouter  from './routes/twiterRouter.js';
+import TTRouter  from './routes/tiktokRouter.js';
+import InstagramRouter  from './routes/Instagram.js';
+import FaceBookRouter  from './routes/faceBook.js';
 
-dotenv.config();
-
-const YTRouter = require('./routes/youtubeRouter.js');
-const TWTRouter = require('./routes/twiterRouter.js');
-const TTRouter = require('./routes/tiktokRouter.js');
-const InstagramRouter = require('./routes/Instagram.js');
-const FaceBookRouter = require('./routes/faceBook.js');
-
+import cors  from 'cors';
+dotenv.config()
 const app = express();
 const Port = process.env.PORT || 8000;
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.get('/', (req, res) => {
-  res.send('your APIs are working  Happy Coding ......');
+  res.send('your APIs are working  Happy Coding ......')
 });
-
-app.use('/api/tiktok', TTRouter);
-app.use('/api/youtube', YTRouter);
-app.use('/api/twitter', TWTRouter);
-app.use('/api/instagram', InstagramRouter);
-app.use('/api/facebook', FaceBookRouter);
-
+app.use('/api/tiktok',TTRouter);
+app.use('/api/youtube', YTRouter); //https://www.youtube.com/watch?v=0HuOyleXVwk
+app.use('/api/twitter',TWTRouter);
+app.use('/api/instagram',InstagramRouter);
+app.use('/api/facebook',FaceBookRouter)
 app.listen(Port, () => {
   console.log(`Server is running on http://localhost:${Port}`);
 });
+export default app;
+// module.exports = app;
